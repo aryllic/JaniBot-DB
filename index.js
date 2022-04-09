a24936ea8448a15e489cd45e373cf317ef08e3fd
Jan <59052885+aryllic@users.noreply.github.com>
2022-04-09 22:36:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const client = new discord.Client({intents: [
 ]});
 
 const commands = require("./commands.js");
-const ytdl = require("ytdl-core");
-const ytSearch = require("yt-search");
 
 client.on("ready", function() {
     console.log("Bot started!");
